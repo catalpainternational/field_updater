05adccd566b037df8adc927915b3be72adbb0f20
PeteCoward <peter@catalpa.io>
2020-07-11 10:56:00 +0900

    Field updater working against example url

--- static/js/field-updater.js
+++ static/js/field-updater.js
@@ -1,20 +1,56 @@
 export default function initialise(options) {
+    // gets a reference to the containing element for the component
     const container = document.querySelector('#' + options.prefix + '-' + options.instance_id);
+
     const display = container.querySelector('.' + options.prefix + '-display');
+    const inputs = container.querySelector('.' + options.prefix + '-inputs');
     const input = container.querySelector('.' + options.prefix + '-input');
     const clear = container.querySelector('.' + options.prefix + '-clear');
     const submit = container.querySelector('.' + options.prefix + '-submit');
     const cancel = container.querySelector('.' + options.prefix + '-cancel');
+    const loader = container.querySelector('.' + options.prefix + '-loader');
+    const error = container.querySelector('.' + options.prefix + '-error');
 
 
+    display.onclick = function(e) {
+        inputs.style.display = 'block';
+        display.style.display = 'none';
+        input.value = display.innerHTML;
+        error.style.display = 'none';
+    };
     clear.onclick = function(e) {
         alert('clearing');
     };
     cancel.onclick = function(e) {
-        alert('cancelling');
+        inputs.style.display = 'none';
+        display.style.display = 'block';
     };
     submit.onclick = function(e) {
-        const newValue = input.value;
-        alert('submitting ' + newValue);
+        // hide the inputs
+        inputs.style.display = 'none';
+        // show the loader
+        loader.style.display = 'block';
+
+        // make our request to udpate the value
+        fetch('/example_submit').then(function(response) {
+            // function that will run on promise resolve;
+            if( response.ok ) {
+                display.innerHTML = input.value;
+            } else {
+                error.innerHTML = response.statusText;
+                error.style.display = 'block';
+            }
+        }).catch(function(err) {
+            // function that will run on promise reject;
+            error.innerHTML = err;
+            error.style.display = 'block';
+        }).finally(function() {
+            // function that will run  always once the promise has resolved or rejected
+            inputs.style.display = 'none';
+            display.style.display = 'block';
+            loader.style.display = 'none';
+
+        });
+
     };
 };
--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import submit
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='field_updater/example.html')),
+    path('example_submit', submit),
 ]
