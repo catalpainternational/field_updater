6cf3dddfb5780331d39de937acd3134230ff73f2
Kevin Keller <flygeneticist@gmail.com>
2020-07-24 11:24:55 +0800

    headers wired up, clean up fetches & imports

--- static/js/ajaxUpdate.js
+++ static/js/ajaxUpdate.js
@@ -1,40 +1,37 @@
-export async function ajaxDelete(options) {
+import getCsrfToken from './getCsrfToken.js';
+
+export function ajaxDelete(options) {
     const fetchInit = {
         method: 'DELETE',
+        headers: {
+            'X-CSRFToken': getCsrfToken(),
+            'Accept': options.headersAccept,
+            'Content-Type': options.headersContentType,
+        },
     };
-    return ajax(options.submit_url, fetchInit);    
+    return ajax(options.submit_url, fetchInit);
 }
-export async function ajaxUpdate(data, options) {
+
+export function ajaxUpdate(data, options) {
     const fetchInit = {
         method: 'POST',
+        headers: {
+            'X-CSRFToken': getCsrfToken(),
+            'Accept': options.headersAccept,
+            'Content-Type': options.headersContentType,
+        },
         body: BODY_ENCODE_FUNCTIONS[options.bodyEncode](data),
     };
-    return ajax(options.submit_url, fetchInit);    
+    return ajax(options.submit_url, fetchInit);
 }
 
 async function ajax(url, fetchInit) {
-    let {default:getCsrfToken} = await import('./getCsrfToken.js');
-    // make our request to udpate the value
-    return new Promise((resolve, reject) => {
-        
-        fetch(
-            url,
-            Object.assign({
-                headers: {
-                    'X-CSRFToken': getCsrfToken(),
-                }
-            }, fetchInit)
-        ).then(function(response) {
-            // function that will run on promise resolve;
-            if(response.ok) {
-                resolve(response.status, response);
-            } else {
-                reject(response.statusText, response.status, response)
-            }
-        }).catch(function(err) {
-            reject(err.message, err)
-        });
-    });
+    const response = await fetch(url, fetchInit)
+        .then(res => res.json()) // pull out JSON from response returned
+        .then(json => console.log(json)) // json only returns {"status": "ok"} if all is well
+        .catch(err => console.log(err));
+
+    return response;
 }
 
 function urlEncode(data) {
@@ -42,10 +39,8 @@ function urlEncode(data) {
 }
 
 function formEncode(data) {
-    let formData = new FormData();
-    Object.keys(data).forEach((key) => {
-        formData.set(key, data[key]);
-    });
+    const formData = new FormData();
+    Object.keys(data).forEach(key => formData.append(key, data[key]));
     return formData;
 }
 
