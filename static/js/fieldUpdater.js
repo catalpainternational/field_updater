90535d1970d0377a28362922e94755dc731b228a
PeteCoward <peter@catalpa.io>
2020-07-29 10:38:58 +0900

    Allow input attributes control

--- static/js/fieldUpdater.js
+++ static/js/fieldUpdater.js
@@ -1,41 +1,39 @@
-export default async function initialise(options) {
+export default async function initialise(config) {
     // gets a reference to the containing element for the component
-    const containerElement = document.getElementById(options.prefix + '-' + options.instance_id);
+    const containerElement = document.getElementById(config.options.prefix + '-' + config.instanceId);
 
     // gets references to internal elements
-    const displayElement = containerElement.querySelector('.' + options.prefix + '-display');
-    const inputsElement = containerElement.querySelector('.' + options.prefix + '-inputs');
-    const inputElement = containerElement.querySelector('.' + options.prefix + '-input');
-    const submitElement = containerElement.querySelector('.' + options.prefix + '-submit');
-    const cancelElement = containerElement.querySelector('.' + options.prefix + '-cancel');
-    const deleteElement = containerElement.querySelector('.' + options.prefix + '-delete');
-    const loaderElement = containerElement.querySelector('.' + options.prefix + '-loader');
-    const errorElement = containerElement.querySelector('.' + options.prefix + '-error');
+    const displayElement = containerElement.querySelector('.' + config.options.prefix + '-display');
+    const inputsElement = containerElement.querySelector('.' + config.options.prefix + '-inputs');
+    const inputElement = containerElement.querySelector('.' + config.options.prefix + '-input');
+    const submitElement = containerElement.querySelector('.' + config.options.prefix + '-submit');
+    const cancelElement = containerElement.querySelector('.' + config.options.prefix + '-cancel');
+    const deleteElement = containerElement.querySelector('.' + config.options.prefix + '-delete');
+    const loaderElement = containerElement.querySelector('.' + config.options.prefix + '-loader');
+    const errorElement = containerElement.querySelector('.' + config.options.prefix + '-error');
 
     const { default: StorageHandler } = await import("./ajaxUpdate.js");
 
-    let current_value = options.attribute_value;
+    let currentValue = config.attributeValue;
     const storage = new StorageHandler({
-        url: options.submit_url,
-        ifUnmodifiedSince: options.if_unmodified_since,
-        ifMatch: options.if_match,
-        bodyEncode: options.bodyEncode,
-        customHeaders: {
-            'Accept': options.headersAccept,
-        },
+        url: config.submitUrl,
+        ifUnmodifiedSince: config.ifUnmodifiedSince,
+        ifMatch: config.ifMatch,
+        bodyEncode: config.options.bodyEncode,
+        customHeaders: config.options.headers,
     });
 
     // set the display back to default
     function updateDisplay() {
-        deleteElement.hidden = !options.allow_delete || current_value === null;
-        displayElement.innerHTML = current_value || options.emptyDisplay;
+        deleteElement.hidden = !config.options.allowDelete || currentValue === null;
+        displayElement.innerHTML = currentValue || config.options.emptyDisplay;
     };
     updateDisplay();
 
     // what happens when the default value display is clicked
     displayElement.onclick = function(e) {
         // show and update the input elements
-        inputElement.value = current_value;
+        inputElement.value = currentValue;
         inputsElement.hidden = false;
         // hide the display elements
         displayElement.hidden = true;
@@ -72,7 +70,7 @@ export default async function initialise(options) {
     deleteElement.onclick = async function(e) {
         submit(() => { 
             return storage.remove().then(() => {
-                current_value = null;
+                currentValue = null;
             }); 
         });
     };
@@ -89,11 +87,11 @@ export default async function initialise(options) {
         let { ajaxUpdate } = await import("./ajaxUpdate.js");
 
         const data = {};
-        data[options.attribute_name] = newValue;
+        data[config.attributeName] = newValue;
 
         submit(() => { 
             return storage.updateOrCreate(data).then(() => {
-                current_value = newValue;
+                currentValue = newValue;
             }); 
         });
     }
--- views.py
+++ views.py
@@ -1,18 +1,100 @@
-from django.http import HttpResponse
+import logging
+
+from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.views.generic import TemplateView
+from django.views.decorators.http import condition
+from django.views.generic import View, TemplateView
+from django.views.generic.detail import SingleObjectMixin
+
+logger = logging.getLogger('django.field_updater')
 
 
 @method_decorator(ensure_csrf_cookie, name='dispatch')
 class ExampleView(TemplateView):
-    ''' An endpoint to provide exmaple use of this apps components '''
+    ''' An endpoint to provide example use of this apps components '''
     template_name = "field_updater/example.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-def submit(request, *args, **kwargs):
-    ''' An example submit view that simply returns 200 OK '''
-    return HttpResponse()
+        context['updater_options_number'] = dict(
+            inputAttributes=dict(type='number'),
+        )
+        context['updater_options_delete'] = dict(
+            allowDelete=True,
+        )
+        context['updater_options_headers'] = dict(
+            headers=dict(yourHeader='yourHeaderValue'),
+        )
+        return context
 
-# TODO add more example edpoint tags to return other statuses for testing error behavior
+
+class LoggingSubmitView(View):
+    ''' An example submit view that does nothing just logs usefully '''
+    log_headers = ['Content-Type', 'Accept', 'IfMatch', 'IfUnmodifiedSince']
+
+    def post(self, request, *args, **kwargs):
+        ''' update or create your stored value '''
+        logger.info(request)
+        for key in self.log_headers:
+            if key in request.headers:
+                logger.info("{}: {}".format(key, request.headers[key]))
+        logger.info(request.POST)
+        return HttpResponse()
+
+    def delete(self, request, *args, **kwargs):
+        ''' delete your stored value '''
+        logger.info(request)
+        for key in self.log_headers:
+            if key in request.headers:
+                logger.info("{}: {}".format(key, request.headers[key]))
+        return HttpResponse()
+
+
+class ModelSubmitView(SingleObjectMixin, View):
+    ''' A simple field updater based on django SingleObjectMixin
+    get: returns json serialized model fields
+    post:
+      - view has no kwargs: creates with request POST dict
+      - view has kwargs: update/create using view kwargs to get, POST dict to update
+    delete: deletes the model instance
+    '''
+
+    def get(self, request, *args, **kwargs):
+        ''' gets the object from storage and returns JSON serialization '''
+        self.object = super().get_object()
+        data = self.object.__dict__.copy()
+        data.pop('_state')
+        return JsonResponse(data)
+
+    def post(self, request, *args, **kwargs):
+        ''' gets the object from storage and updates it '''
+        # todo break out method to allow GET params as unique identifiers
+        object_unique = kwargs
+        if len(object_unique):
+            # if we have identifying data, update/create
+            self.object, created = self.model.objects.update_or_create(
+                defaults=request.POST,
+                **object_unique
+            )
+        else:
+            # otherwise create
+            created = True
+            self.object = self.model.objects.create(**request.POST.dict())
+
+        response = HttpResponse(status=201 if created else 200)
+
+        # set a location header if created
+        if created and self.get_instance_url:
+            # TODO get_absolute_url() ?
+            response['Location'] = self.get_instance_url(self.object)
+
+        return response
+
+    def delete(self, request, *args, **kwargs):
+        ''' get and delete '''
+        self.object = super().get_object()
+        self.object.delete()
+        return HttpResponse(status=200)
 
