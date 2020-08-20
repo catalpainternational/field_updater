94ce30f7888c584637a16df4321e36d94e9daa20
PeteCoward <peter@catalpa.io>
2020-08-20 11:48:17 +0900

    Namespacing urls

--- urls.py
+++ urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import LoggingSubmitView, ExampleView
 
+app_name = 'field_updater'
 urlpatterns = [
     # an example endpoint to demonstrate component use
-    path('', ExampleView.as_view()),
+    path('', ExampleView.as_view(), name='examples'),
     # a simple example submit endpoint that returns 200 ok
     path('example_submit', LoggingSubmitView.as_view(), name='example_submit'),
     # an example submit endpoint with a parameter that returns 200 ok
